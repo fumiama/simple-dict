ed57e18b0518fb0e93c438323fc4248ef0ef2495
fumiama <s862105088@gmail.com>
2021-12-08 23:09:08 +0800

    改正顺序

--- server.c
+++ server.c
@@ -382,7 +382,7 @@ static void handle_accept(void *p) {
                 printf("Get %u bytes: %s\n", timer_pointer_of(p)->numbytes, buff);
                 puts("Check buffer");
                 take_word(p, cfg->pwd);
-                take_word(p, setpass) else take_word(p, delpass) else take_word(p, "cat") else take_word(p, "md5");
+                take_word(p, setpass) else take_word(p, delpass);
                 if(timer_pointer_of(p)->numbytes > 0) chkbuf(p);
             }
             printf("Break: recv %u bytes\n", timer_pointer_of(p)->numbytes);
